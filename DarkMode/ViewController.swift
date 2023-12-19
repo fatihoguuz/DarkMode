69998bf04d5764db91ac4a9c1725d280c141ea12
Fatih Oguz <oguzfatih39@gmail.com>
2023-12-19 22:47:02 +0300

    completed

--- DarkMode/ViewController.swift
+++ DarkMode/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nextButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      
+        
+       // overrideUserInterfaceStyle = .light
     }
 
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let userInterfaceStayle = traitCollection.userInterfaceStyle
+        
+        if userInterfaceStayle == .dark {
+            nextButton.tintColor = UIColor.white
+        }else {
+            nextButton.tintColor = UIColor.blue
+        }
+    }
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        let userInterfaceStayle = traitCollection.userInterfaceStyle
+        
+        if userInterfaceStayle == .dark {
+            nextButton.tintColor = UIColor.white
+        }else {
+            nextButton.tintColor = UIColor.blue
+        }
+    }
 }
 
